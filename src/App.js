8be41f320eaa212a33112f86497cf9aec7266688
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-18 16:01:03 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => <PersonList />;
+
+const PersonList = () => <Person />;
+
+const Person = () => {
+  const url = 'https://randomuser.me/api/portraits/thumb/men/22.jpg';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="person">
+      <img src={url} alt="person" />
+      <h4>Name</h4>
+      <h4>Job</h4>
     </div>
   );
-}
+};
 
 export default App;
