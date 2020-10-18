5218f3a0e9a9f52b8330cfde3558b623931ea348
Mahabubur Rahman <mahabuburr01625@gmail.com>
2020-10-18 16:21:27 +0600

    completed the first version

--- src/App.js
+++ src/App.js
@@ -3,15 +3,49 @@ import './App.css';
 
 const App = () => <PersonList />;
 
-const PersonList = () => <Person />;
+const PersonList = () => {
+  const people = [
+    {
+      img: 22,
+      name: 'Jhon',
+      job: 'Developer',
+    },
+    {
+      img: 23,
+      name: 'Jhok',
+      job: 'Designer',
+    },
+    {
+      img: 24,
+      name: 'Rony',
+      job: 'Artist',
+    },
+  ];
 
-const Person = () => {
-  const url = 'https://randomuser.me/api/portraits/thumb/men/22.jpg';
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]}>
+        Lorem ipsum dolor sit amet consectetur adipiscing elit feugiat, egestas
+        neque venenatis sapien erat mus cubilia pharetra, nisi mauris leo auctor
+        natoque pellentesque penatibus.
+      </Person>
+      <Person person={people[2]} />
+    </section>
+  );
+};
+
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+
+  const url = `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
   return (
     <div className="person">
       <img src={url} alt="person" />
-      <h4>Name</h4>
-      <h4>Job</h4>
+      <h4>{name}</h4>
+      <h4>{job}</h4>
+      {children}
     </div>
   );
 };
